d80cb62fc4b12302fb9da890e83b2a216d4905e2
Christian Lapinig <clapinig@adobe.com>
2024-10-18 09:37:19 -0700

    Add onedark colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+-- local colorscheme = require "lazyvim.plugins.colorscheme"
+return {
+  { "navarasu/onedark.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
